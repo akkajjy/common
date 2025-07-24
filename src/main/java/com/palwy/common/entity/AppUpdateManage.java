91faa35021d6ad56f568e9ad403cea53345d1798
CHENSONG HU <782815280@qqcom>
2025-07-24 20:33:30 +0800

    更新管理——增删改查+检查强制更新状态

--- src/main/java/com/palwy/common/entity/AppUpdateManage.java
+++ src/main/java/com/palwy/common/entity/AppUpdateManage.java
@@ -1,80 +1,66 @@
 package com.palwy.common.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
 public class AppUpdateManage implements Serializable {
+
     @ApiModelProperty("主键ID")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long id;
 
-
     @ApiModelProperty("应用名称")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotBlank(message = "应用名称不能为空")
     private String appName;
 
-    @Valid
     @ApiModelProperty("版本号")
     @NotBlank(message = "版本号不能为空")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String versionCode;
 
-    @Valid
     @ApiModelProperty("版本名称")
     @NotBlank(message = "版本名称不能为空")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String versionName;
 
-    @Valid
     @ApiModelProperty("平台")
     @NotBlank(message = "发版平台不能为空")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String platform;
 
-    @Valid
     @ApiModelProperty("强制更新类型(0:不更新 1:不强制更新 2:强制更新)")
-    @NotBlank(message = "强制更新类型不能为空")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "强制更新类型不能为空")
+    @Pattern(regexp = "^[012]$", message = "强制更新类型值不合法，只能是0、1或2")
     private String forceUpdateType;
 
-    @Valid
     @ApiModelProperty("最低支持版本号")
     @NotBlank(message = "最低支持版本号不能为空")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String lowVersionCode;
 
     @ApiModelProperty("更新描述")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String updateDesc;
 
     @ApiModelProperty("创建人")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String creator;
 
     @ApiModelProperty("创建时间")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gmtCreated;
 
     @ApiModelProperty("修改人")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String modifier;
 
     @ApiModelProperty("修改时间")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gmtModified;
 
     @ApiModelProperty("删除标识(N:未删除 Y:已删除)")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    private String isDeleted;
+    @Pattern(regexp = "^[NY]$", message = "删除标识值不合法，只能是N或Y")
+    private String isDeleted = "N"; // 默认值
 
     @ApiModelProperty("系统类型(1:Android 2:iOS)")
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "系统类型不能为空")
+    @Pattern(regexp = "^[12]$", message = "系统类型值不合法，只能是1或2")
     private String osType;
 }
