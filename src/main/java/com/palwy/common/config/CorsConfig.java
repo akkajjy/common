6d6d9935532da22b255c7673c96e944751a8f90d
CHENSONG HU <782815280@qqcom>
2025-07-30 13:28:42 +0800

    解决跨域

---
+++ src/main/java/com/palwy/common/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.palwy.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://one-in-test.shhpalwy.com") // 允许的前端域名
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+                .allowedHeaders("*") // 允许所有请求头
+                .allowCredentials(true) // 允许携带凭证
+                .maxAge(3600); // 预检请求缓存时间（秒）
+    }
+}
