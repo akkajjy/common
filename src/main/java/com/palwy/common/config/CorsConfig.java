28b24981eed1b8fdd6ea8af7e9b9947532be5f9c
CHENSONG HU <782815280@qqcom>
2025-07-30 15:26:36 +0800

    解决跨域

--- src/main/java/com/palwy/common/config/CorsConfig.java
+++ src/main/java/com/palwy/common/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://one-in-test.shhpalwy.com") // 允许的前端域名
+                .allowedOrigins("http://one-in-test.shhpalwy.com","http://192.168.1.144:9529") // 允许的前端域名
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
                 .allowedHeaders("*") // 允许所有请求头
                 .allowCredentials(true) // 允许携带凭证
