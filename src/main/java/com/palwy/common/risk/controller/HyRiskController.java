f98ecf097b1f256ac26491d984a8a9e50a709431
CHENSONG HU <782815280@qqcom>
2025-08-26 16:52:58 +0800

    华翊，优鉴联登接口

--- src/main/java/com/palwy/common/risk/controller/HyRiskController.java
+++ src/main/java/com/palwy/common/risk/controller/HyRiskController.java
@@ -7,7 +7,6 @@ import com.palwy.common.util.ResultVOUtil;
 import com.palwy.common.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
