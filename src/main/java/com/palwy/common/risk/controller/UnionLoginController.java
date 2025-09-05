eafa804afb4729c9743e7f323caf6bec9bfca66b
CHENSONG HU <782815280@qqcom>
2025-09-05 16:53:56 +0800

    联合登录

--- src/main/java/com/palwy/common/risk/controller/UnionLoginController.java
+++ src/main/java/com/palwy/common/risk/controller/UnionLoginController.java
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(
         origins = {
                 "https://installment.shhpalwy.com",
-                "http://h5-installment-shop-test.shhpalwy.com",
+                "https://h5-installment-shop-test.shhpalwy.com",
                 "http://localhost:8080",
-                "http://zy-shop-test02.palwy.com"
+                "https://zy-shop-test02.palwy.com"
         },
         allowCredentials = "true"
 )
